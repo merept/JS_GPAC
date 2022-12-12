a2a3205f5178ffc252455189eda90a3e3740fdd7
merept <1466005267@qq.com>
2022-12-12 14:15:48 +0800

    update comments of JS_GPAC

--- JS_GPAC.js
+++ JS_GPAC.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SWPU绩点计算
 // @namespace    http://merept.github.io/
-// @version      1.2.7
+// @version      1.2.8
 // @license      MIT
 // @description  在jwxt.swpu.edu.cn的“综合查询-全部成绩”以及“本学期成绩”页面显示各个学期的平均学分绩点，加粗并打上“※”号的课程是计算进去的，有“（跳过）”注释的证明是英语四六级、选修课或暂时未出成绩的课程，不计算在内（若要计算选修课，请把源代码最上面的skipElectives变量的值改为false），什么标记都没有的可能是没有计算进去，刷新网页即可。（结果可能有出入，仅供参考）
 // @author       MerePT
@@ -9,29 +9,67 @@
 // @icon         https://pic.imgdb.cn/item/6388c62816f2c2beb1c0909d.png
 // @grant        unsafeWindow
 // ==/UserScript==
+/**
+ * @param {boolean} 是否跳过选修课
+ */
 var skipElectives = true; //true: 不计算选修课，false: 计算选修课，默认不计算选修课
 
+/**
+ * 总学分
+ */
 var totalPoints = [];
+/**
+ * 总成绩
+ */
 var totalScores = [];
 
+/**
+ * 判断是否为 CET 相关的课程
+ * @param {String} name 课程名称
+ * @returns {boolean} true: 是 CET 相关课程; false: 不是
+ */
 function isEnglishTest(name) {
     return new RegExp("英语实践+").test(name)
             || new RegExp("全国英语+").test(name);
 }
 
+/**
+ * 判断是否为选修课, 若用户设置变量 {skipElectives} 为 false 则直接返回 false
+ * @param {String} course 课程号
+ * @returns {boolean} true: 是选修课; false: 不是或者用户设置不跳过选修课
+ */
 function isElectives(course) {
     return skipElectives && course.substring(0, 2) == '00';
 }
 
-function isEffectiveScore(str) {
-    return !str || new RegExp("^[\s　]*$").test(str);
+/**
+ * 判断成绩是否为空
+ * @param {String} score 课程成绩
+ * @returns {boolean} true: 课程成绩为空; false: 不为空
+ */
+function isEffectiveScore(score) {
+    return !score || new RegExp("^[\s　]*$").test(score);
 }
 
+/**
+ * 判断该课程是否计入绩点
+ * @param {String} name 课程名称
+ * @param {String} course 课程号
+ * @param {String} score 课程成绩
+ * @returns {boolean} true: 不计入该课程成绩; false: 计入
+ */
 function isNotCount(name, course, score) {
     return isEnglishTest(name) || isElectives(course) || isEffectiveScore(score);
 }
 
-function calGp(obj, ps, sPlace) {
+/**
+ * 计算单个学期的平均学分绩点
+ * @param {HTMLAllCollection} obj 该学期的 HTML 集合
+ * @param {object} ps 存有总学分和总成绩的对象
+ * @param {number} sPlace 成绩所在的位置
+ * @returns {object} 存有总学分和总成绩的对象
+ */
+function calSingleGpa(obj, ps, sPlace) {
     for (let o of obj) {
         let datas = o.querySelectorAll('td');
         if (isNotCount(datas[2].innerText, datas[0].innerText, datas[sPlace].innerText)) {
@@ -57,6 +95,11 @@ function calGp(obj, ps, sPlace) {
     return ps;
 }
 
+/**
+ * 计算平均学分绩点
+ * @param {HTMLIFrameElement} frame 记录了课程信息的网页框架
+ * @param {number} sPlace 成绩所在的位置
+ */
 function calGpa(frame, sPlace) {
     let ps = {
         tp: 0,
@@ -66,13 +109,13 @@ function calGpa(frame, sPlace) {
 
     for (let s of scores) {
         let even = s.querySelectorAll('.even');
-        ps = calGp(even, ps, sPlace);
+        ps = calSingleGpa(even, ps, sPlace);
 
         let ef = s.querySelectorAll('.evenfocus');
-        ps = calGp(ef, ps, sPlace);
+        ps = calSingleGpa(ef, ps, sPlace);
 
         let odd = s.querySelectorAll('.odd');
-        ps = calGp(odd, ps, sPlace);
+        ps = calSingleGpa(odd, ps, sPlace);
 
         totalPoints.push(ps.tp);
         totalScores.push(ps.ts);
@@ -87,31 +130,60 @@ function calGpa(frame, sPlace) {
 
 
 function main() {
+    // “综合查询 - 全部成绩”页面的 GPA 显示
     setInterval(function() {
         if (document.querySelector("frame[name='bottomFrame']").contentWindow.document.querySelector("#mainF").contentWindow.document.querySelector("iframe[name='lnqbIfra']") != null) {
             if (document.querySelector("frame[name='bottomFrame']").contentWindow.document.querySelector("#mainF").contentWindow.document.querySelector("iframe[name='lnqbIfra']").contentWindow == undefined) {
                 return;
             }
 
+            /**
+             * main 框架
+             */ 
             let mainF = document.querySelector("frame[name='bottomFrame']").contentWindow
                         .document.querySelector("#mainF").contentWindow;
+            /**
+             * 全部成绩页面内的 lnqbIfra 框架
+             */
             let scoreF = mainF.document.querySelector("iframe[name='lnqbIfra']").contentWindow;
 
             if (scoreF.document.body != null && scoreF.document.body.getAttribute('is-calculated') == undefined && scoreF.document.querySelectorAll('.title').length != 0) {
                 // clearInterval(timer);
-                scoreF.document.body.setAttribute("is-calculated", "true");
+                scoreF.document.body.setAttribute("is-calculated", "true"); // 设置属性避免延时时持续对框架内容进行操作
 
+                // 延时等待框架加载完成
                 setTimeout(function() {
                     calGpa(scoreF, 6);
 
+                    /**
+                     * 每学期的标题位置
+                     */
                     let titles = scoreF.document.querySelectorAll('.title');
-                    let pa = scoreF.document.querySelectorAll('.pageAlign');
-                    let headTitle = mainF.document.querySelector('#tblHead');
+                    /**
+                     * 每学期底部的汇总信息位置
+                     */
+                    let pa = scoreF.document.querySelectorAll('.pageAlign'); 
+                    /**
+                     * “历年成绩”标题位置
+                     */
+                    let headTitle = mainF.document.querySelector('#tblHead'); 
+                    /**
+                     * 侧边栏各学期跳转链接位置
+                     */
                     let slide = mainF.document.querySelector('.table_k')
                                                 .querySelectorAll('table')[7]
-                                                .querySelectorAll('a');
-                    let whole = {
+                                                .querySelectorAll('a'); 
+                    /**
+                     * 全部成绩
+                     */
+                    let total = {
+                        /**
+                         * 学分
+                         */
                         points: 0,
+                        /**
+                         * 绩点
+                         */
                         scores: 0
                     };
 
@@ -129,11 +201,11 @@ function main() {
                         slide[i].innerText += slide[i].getAttribute('is-add')==undefined ? ("\xa0\xa0GPA: " + gpa) : '';
                         slide[i].setAttribute('is-add', 'true');
 
-                        whole.points += totalPoints[i];
-                        whole.scores += totalScores[i];
+                        total.points += totalPoints[i];
+                        total.scores += totalScores[i];
                     }
 
-                    let s = "\xa0\xa0GPA: " + (whole.scores / whole.points).toFixed(2);
+                    let s = "\xa0\xa0GPA: " + (total.scores / total.points).toFixed(2);
                     headTitle.querySelectorAll('td')[2]
                                 .querySelector('b').innerText += headTitle.getAttribute('is-add')==undefined ? s : '';
                     headTitle.setAttribute('is-add', 'true');
@@ -145,21 +217,32 @@ function main() {
         }
     },30);
 
+    // “综合查询 - 本学期成绩”页面的 GPA 显示
     setInterval(function() {
         if (document.querySelector("frame[name='bottomFrame']").contentWindow
                 .document.querySelector("#mainF").contentWindow != undefined) 
         {
+            /**
+             * main 框架
+             */
             let mainF = document.querySelector("frame[name='bottomFrame']").contentWindow
                         .document.querySelector("#mainF").contentWindow;
+            /**
+             * 各页面 mainF 框架中的标题位置
+             */
             let titles = mainF.document.querySelectorAll('.title');
 
+            // 当标题位置的 innerText 为“本学期成绩”时执行
             if (mainF.document.body != null && mainF.document.body.getAttribute('is-calculated') == undefined && titles.length != 0 && titles[0].getElementsByTagName('td')[2].innerText.trim() == '本学期成绩查询列表') {
                 // clearInterval(timer);
-                mainF.document.body.setAttribute("is-calculated", "true");
+                mainF.document.body.setAttribute("is-calculated", "true"); // 设置属性避免延时时持续对框架内容进行操作
 
                 setTimeout(function() {
                     calGpa(mainF, 9);
 
+                    /**
+                     * “本学期成绩”标题位置
+                     */
                     let title = mainF.document.querySelector('.title');
 
                     // console.log(titles[i]);
@@ -176,12 +259,10 @@ function main() {
 }
 
 (function() {
-    let isLoaded = false
     let timer = setInterval(function() {
         if (document.querySelector("frame[name='bottomFrame']").contentWindow
                 .document.querySelector("#mainF").contentWindow
                 .document.querySelectorAll("title")[0].innerText === '成绩查询') {
-            isLoaded = true;
             clearInterval(timer);
             main();
         }
